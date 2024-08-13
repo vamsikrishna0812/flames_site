6c4176a4ffd30fbe0a0922f330bc81cf844fe1b8
vamsikrishna0812 <143340843+vamsikrishna0812@users.noreply.github.com>
2024-08-13 12:08:07 +0530

    Update flames_analyzer.py

--- flames_package/flames_analyzer.py
+++ flames_package/flames_analyzer.py
@@ -8,15 +8,12 @@ def flames_analyzer(n1, n2):
             for j in range (2):
                 n.remove(i)
     c = len(n)
-    f = { 1 : "F",
-          2 : "L",
-          3 : "A",
-          4 : "M",
-          5 : "E",
-          6 : "S"
-        }
-    if c <= 6:
-        return f[c]
-    else:
-        return f[(c % 6)]
+    f = ['F', 'L', 'A', 'M', 'E', 'S']
+    
+    while len(f) != 1 :
+        if c % len(f) == 0:
+            f.remove(f[-1])
+        else:
+            f.remove(f[(c % len(f) - 1)])
+    return f[0]
     
